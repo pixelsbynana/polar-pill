6d08a03e409760367819177d3545d30ea54aa135
Nana <musingful@gmail.com>
2026-07-25 16:01:15 +0100

    Initial Commit

---
+++ Polar Pill/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Polar Pill
+//
+//  Created by Arrada Wongkittiruk on 25/07/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Polar PillTests/Polar_PillTests.swift
@@ -0,0 +1,19 @@
+//
+//  Polar_PillTests.swift
+//  Polar PillTests
+//
+//  Created by Arrada Wongkittiruk on 25/07/2026.
+//
+
+import Testing
+@testable import Polar_Pill
+
+struct Polar_PillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
